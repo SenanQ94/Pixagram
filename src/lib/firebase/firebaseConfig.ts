03fe74972faf0f4e1166be4140af32a47bb553cb
Sinan Qarhiely <115164036+SenanQ94@users.noreply.github.com>
2024-01-04 00:01:03 +0100

    Update firebaseConfig.ts

--- src/lib/firebase/firebaseConfig.ts
+++ src/lib/firebase/firebaseConfig.ts
@@ -8,24 +8,13 @@ import { getStorage } from 'firebase/storage';
 // https://firebase.google.com/docs/web/setup#available-libraries
 
 // Your web app's Firebase configuration
-// For Firebase JS SDK v7.20.0 and later, measurementId is optional
-// const firebaseConfig = {
-//   apiKey: "AIzaSyAUjQ4i3s2CG88ZGZRm74HrH78dscR2X40",
-//   authDomain: "pixagram-a4a1b.firebaseapp.com",
-//   projectId: "pixagram-a4a1b",
-//   storageBucket: "pixagram-a4a1b.appspot.com",
-//   messagingSenderId: "770906740157",
-//   appId: "1:770906740157:web:fda4aa040429d1d5cbaed4",
-//   measurementId: "G-30GZP60PP2"
-// };
-// Your web app's Firebase configuration
 const firebaseConfig = {
-  apiKey: "AIzaSyDBY9IidR0Ccewq543LQFysW4LTkP4l0Yk",
-  authDomain: "pixagram-a0161.firebaseapp.com",
-  projectId: "pixagram-a0161",
-  storageBucket: "pixagram-a0161.appspot.com",
-  messagingSenderId: "613088713180",
-  appId: "1:613088713180:web:cdd2eaa767b3d4eb105719"
+  apiKey: "xxxxxxxxxxxxxxxxxxxxxxx",
+  authDomain: "xxxxxxxxxxxxx",
+  projectId: "xxxxxxxxxxx",
+  storageBucket: "xxxxxxxxxxxxxxx",
+  messagingSenderId: "xxxxxxxxxxxxxxx",
+  appId: "xxxxxxxxxxxxxxxxx"
 };
 
 // Initialize Firebase
